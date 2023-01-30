3a524bbd8c0852011d7aaf1c62c921abae0b90e7
kunalupadhyay70 <upadhyaykunal70@gmail.com>
2023-01-30 17:58:49 +0530

    Update diogonals.java

--- diogonals.java
+++ diogonals.java
@@ -10,7 +10,25 @@ class diagonals{
         for(int i=0;i<n;i++)     //for outer body count at row wise 
         {
             for(int j=0;j<n;j++)   // count as coloumn wise 
-            { if(i==j || i+j==n-1 || i+j== (n-1)/2 || i-j==(n-1)/2 || i+j==n-1+ (n-1)/2 ||j-i==(n-1)/2)
+            { if(i==j ||i+j==n-1 || i+j== (n-1)/2 || i-j==(n-1)/2 || i+j==n-1+ (n-1)/2 ||j-i==(n-1)/2)
+                {
+                    System.out.print("*");
+                }
+                else
+                System.out.print(" ");
+
+            } System.out.print("     ");
+            // printing the 'A'
+            for(int j=0;j<n;j++){
+                if ((i==0 && j>0 && j<(n-1)/2)||(j==0 &&i>0)|| (j==(n-1)/2 && i>0) ||(i==(n-1)/2 && j<=(n-1)/2) ){
+                   System.out.print("*");
+                }
+                else
+                System.out.print(" ");
+              }
+            for(int j=0;j<n;j++)   // for the triangle 
+            {  
+              if( i+j== (n-1)/2||j-i==(n-1)/2 ||i==(n-1)/2)
                 {
                     System.out.print("*");
                 }
