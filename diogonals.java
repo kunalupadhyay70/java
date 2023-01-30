e6e170f1ca5f793681ac71c6348111399a8a6bee
kunalupadhyay70 <upadhyaykunal70@gmail.com>
2023-01-30 22:58:47 +0530

    filesadded

--- diogonals.java
+++ diogonals.java
@@ -14,11 +14,11 @@ class diagonals{
                 {
                     System.out.print("*");
                 }
-                else
+                else                            
                 System.out.print(" ");
 
             } System.out.print("     ");
-            // printing the 'A'
+            // printing the 'A'alphabet
             for(int j=0;j<n;j++){
                 if ((i==0 && j>0 && j<(n-1)/2)||(j==0 &&i>0)|| (j==(n-1)/2 && i>0) ||(i==(n-1)/2 && j<=(n-1)/2) ){
                    System.out.print("*");
