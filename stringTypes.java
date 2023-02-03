51a657e6710f0f751fc3421a78f1d074b085b39a
kunalupadhyay70 <upadhyaykunal70@gmail.com>
2023-02-03 22:18:41 +0530

    strings types

--- stringTypes.java
+++ stringTypes.java
@@ -1,10 +1,14 @@
 // there are two types of the string mutable ,immutable 
+import java.io.IOException;
 class stringTypes{
     public static void main( String...args)throws IOException,InterruptedException
-    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); 
-    String brand ="kunal";
+   {new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); 
+    String brand ="kunal";//immutable string 
     System.out.println(brand);
-    StringBuilder brand1= new StringBuilder();
-    brand1 ="kunal";
+    brand.concat(" expert");//cant change the original string 
+    System.out.println(brand);
+    StringBuilder brand1= new StringBuilder("kunal"); //mutable 
     System.out.println(brand1);
-}
+    brand1.append(" expert"); //the changes will reflect in the object
+    System.out.println(brand1);
+}}
