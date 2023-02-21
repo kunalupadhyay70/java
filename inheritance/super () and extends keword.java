114a4102002e096f87e9a319f2b0fa9cff89df1e
kunalupadhyay70 <upadhyaykunal70@gmail.com>
2023-02-21 13:30:41 +0530

    inheritance

---
+++ inheritance/super () and extends keword.java
@@ -0,0 +1,23 @@
+class human{
+private String sex ; //private memeber cant be accesed by derived class #encapsulation
+int age;
+human(){
+System.out.println("human class constructor hu mai");
+}
+void show(){
+    System.out.println("the age is  "+ age );
+}
+}
+class student extends human{
+    student(){      // by defaut added in the class when the object of the class is created
+        super();  //default add super method of 0 parameterised
+    }
+
+}
+class exeute{
+    public static void main(String[] args) {
+        student stud = new student ();
+        stud.age=5;
+        stud.show();
+    }
+}
