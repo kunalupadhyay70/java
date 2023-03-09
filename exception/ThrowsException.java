362a84dda31e0e147edd2a0ed0994f2f15b3b589
kunalupadhyay70 <upadhyaykunal70@gmail.com>
2023-03-09 17:35:21 +0530

    throws

---
+++ exception/ThrowsException.java
@@ -0,0 +1,23 @@
+class demo{
+  void a()throws Exception{
+        b();
+    }
+    void b()throws Exception
+    {
+     int n1=6,n2=0;
+      int res =n1/n2;
+      System.out.println(res);
+    }
+}
+public class ThrowsException{
+    public static void main(String[] args) {
+      try{
+        demo obj= new demo();
+        obj.a();
+    }
+    catch(Exception e){
+        System.out.println("error"+e);
+    }
+}
+
+}
